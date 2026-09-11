6649581b4fe6f4dc7b2b18a950dc4187611d6577
elina-1201 <elina.mursalimova16@gmail.com>
2026-09-11 22:53:53 +0200

    Stage 0: hello server

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,35 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { afterEach, beforeEach, describe, it } from 'node:test';
+import request from 'supertest';
+import { App } from 'supertest/types';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let app: INestApplication<App>;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  afterEach(async () => {
+    await app.close();
+  });
+
+  describe('GET /health', () => {
+    it('should return 200 OK with { status: "ok" }', async () => {
+      await request(app.getHttpServer())
+        .get('/health')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect({ status: 'ok' });
     });
   });
 });
